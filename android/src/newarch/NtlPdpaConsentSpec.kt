8f0cc53281b54c1440c40b94d9557c437a6c537c
suradisworkspace <suradis.workspace@gmail.com>
2023-12-06 11:05:39 +0700

    fix bracket

--- android/src/newarch/NtlPdpaConsentSpec.kt
+++ android/src/newarch/NtlPdpaConsentSpec.kt
@@ -4,4 +4,9 @@ import com.facebook.react.bridge.ReactApplicationContext
 
 abstract class NtlPdpaConsentSpec internal constructor(context: ReactApplicationContext) :
   NativeNtlPdpaConsentSpec(context) {
+
+  abstract fun setGrantAnalyticsStorage(cb: Callback)
+  abstract fun setDenyAnalyticsStorage(cb: Callback)
+  abstract fun setGrantAdStorage(cb: Callback)
+  abstract fun setDenyAdStorage(cb: Callback)
 }
